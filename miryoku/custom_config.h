e3a61652a7c128c5428dd921eef6ade494554581
timatron <timatron@gmail.com>
2022-12-06 22:02:27 -0800

    custom config

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,3 +1,7 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
+#define MIRYOKU_ALPHAS_QWERTY
+#define MIRYOKU_NAV_INVERTEDT
+#define MIRYOKU_CLIPBOARD_MAC
+#define MIRYOKU_MAPPING_EXTENDED_THUMBS
