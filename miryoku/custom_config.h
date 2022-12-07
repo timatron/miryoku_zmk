af1c724696237e5dbbe3467f7b2122b3726b2ddc
timatron <timatron@gmail.com>
2022-12-07 13:04:47 -0800

    hopefully this works!

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,7 +1,8 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
-// #define MIRYOKU_ALPHAS_QWERTY
-// #define MIRYOKU_NAV_INVERTEDT
-// #define MIRYOKU_CLIPBOARD_MAC
-// #define MIRYOKU_MAPPING_EXTENDED_THUMBS
+#define MIRYOKU_ALPHAS_QWERTY
+#define MIRYOKU_NAV_INVERTEDT
+#define MIRYOKU_CLIPBOARD_MAC
+// #define MIRYOKU_LAYERS_FLIP
+#define MIRYOKU_MAPPING_EXTENDED_THUMBS
